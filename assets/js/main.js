1ad333338d776ed69054506212da90c16df0b9e9
parthim <parthim811@gmail.com>
2022-02-10 14:43:00 +0530

    Saass and Javascript added for dynamic site

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,27 @@ for (var i = 0; i < navItem.length; i++) {
     this.className += " active";
   });
 }
+
+// style for Intro Name
+document.getElementById('intro-name').style.cssText ="color:red; font-size:30px;"
+
+// Animation for Skills ProgressBar
+ var widths = document.getElementsByClassName('progress-bar');
+ for(let i=0;i<widths.length;i++){
+  //  document.getElementsByClassName('progress-bar')[i].style.cssText = "width:"+width[i].innerText;
+  let width =widths[i].innerText.replace(/[^0-9]/g, '');
+  console.log(width);
+  var w = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (w >= width) {
+      clearInterval(id);
+    } else {
+      w++; 
+      widths[i].style.width = w + '%';
+      widths[i].innerText = w+'%';
+    }
+  }
+ }
+
 }) ();
